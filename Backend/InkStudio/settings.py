ee33bd5739e709be9327264b368858573e0f238d
Chris Friday <chrisfriday033@gmail.com>
2025-06-05 09:09:56 +0100

    Everywhere good

--- Backend/InkStudio/settings.py
+++ Backend/InkStudio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://tattooapp.onrender.com", "localhost"]
+ALLOWED_HOSTS = ["tattooapp.onrender.com"]
 
 
 # Application definition
